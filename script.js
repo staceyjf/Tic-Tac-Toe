896f615b2aac34c5df9b62440bdcaf17408547e9
staceyjf <staceyfanner@gmail.com>
2023-07-02 12:08:58 +1000

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('working')
 /*----- constants -----*/ 
 const COLORS = {
-    // 'null': '#1035da', 
+    // 'null': 'white', 
     '1': 'x',
     '-1': 'o',
   };
@@ -13,6 +13,8 @@ let winner; // null = no winner; 1 or -1 = winner; 'T' = tie;
 
 /*----- cached elements  -----*/ 
 const messageEl = document.querySelector('h2');
+const playAgainBtn = document.querySelector('button');
+const markerEls = [...document.querySelectorAll('#markers > div')]; 
 
 /*----- Winning combo 1 # winOptR1-----*/
 const winOptR1 = [ 
@@ -64,21 +66,22 @@ const winOptR1 = [
   ]; 
 
 /*----- event listeners -----*/ 
-
+document.getElementById('board').addEventListener('click', handleDrop); // Don't invoke as it doesn't return anything   
+playAgainBtn.addEventListener('click', init);
 
 /*----- functions -----*/ 
 init();
 
 // Initialize all state, then call render ()
 function init() {
-    board = [ 
-        [null, null, null], // col 0
-        [null, null, null], // col 1
-        [null, null, null], // col 2
-      ];
-      turn = 1; // who starts
-      winner = null;
-      render();
+  board = [
+    [null, null, null], // col 0
+    [null, null, null], // col 1
+    [null, null, null], // col 2
+  ];
+  turn = 1; // who starts
+  winner = null;
+  render();
 }
 
 // Visualize all state in the DOM
@@ -88,22 +91,68 @@ function render() {
     renderControls() // show/hide markers/play again button
   }
 
+// In response to user interaction, update all impacted
+// state, then call render();
+function handleDrop(evt) {
+  const { id } = evt.target;
+  const colIdx = parseInt(id.charAt(1));
+  const rowIdx = parseInt(id.charAt(3));
+
+  // Check if the cell is already played or if there's a winner
+  if (board[colIdx][rowIdx] || winner) {
+    return;
+  }
+
+  // Update the board with the player's marker
+  board[colIdx][rowIdx] = turn;
+
+  // Check for a winner
+  winner = getWinner(colIdx, rowIdx);
+
+  // Toggle the turn between players
+  turn *= -1;
+
+  // Render the updated state
+  render();
+}
+
+// Check for winner in board state
+// Return null if no winner, 1/-1 if a player has won, 'T' if tie
+function getWinner(colIdx, rowIdx) {
+  // // Check for a tie
+  // let remainingTiles = 0;
+  // board.forEach(function(colArr) {
+  //   if (!colArr[2]) remainingTiles += 1;
+  // })
+  // if (!remainingTiles) return 'T';
+  // return checkVerticalWin(colIdx, rowIdx) ||
+  //   checkHorizontalWin(colIdx, rowIdx) ||
+  //   checkDiagonalWinNESW(colIdx, rowIdx) ||
+  //   checkDiagonalWinNWSE(colIdx, rowIdx);
+}
+
 function renderBoard() {
-    board.forEach(function(colArr, colIdx) {
-        colArr.forEach(function(cellVal, rowIdx){ // Iterate through current column
-            const cellId = `c${colIdx}r${rowIdx}`;
-            const cellEl = document.getElementById(cellId);
-            if (cellVal === 1) {
-                cellEl.classList.add('x');
-            } else if (cellVal === -1) {
-                cellEl.classList.add('o');
-            } else if (cellVal === 'T') {
-                cellEl.classList.add('tie');
-            } else {
-                cellEl.classList.add('unplayed');
-            }
-        });
+  board.forEach(function(colArr, colIdx) {
+    colArr.forEach(function(cellVal, rowIdx) {
+      const cellId = `c${colIdx}r${rowIdx}`;
+      const cellEl = document.getElementById(cellId);
+      if (cellVal === 1) {
+        cellEl.innerHTML = 'x';
+        cellEl.classList.add('x');
+        cellEl.classList.remove('unplayed');
+      } else if (cellVal === -1) {
+        cellEl.innerHTML = 'o';
+        cellEl.classList.add('o');
+        cellEl.classList.remove('unplayed');
+      } else if (cellVal === 'T') {
+        cellEl.innerHTML = 'T';
+        cellEl.classList.add('tie');
+        cellEl.classList.remove('unplayed');
+      } else {
+        cellEl.classList.add('unplayed');
+      }
     });
+  });
 }
 
 function renderMessage() {
@@ -116,9 +165,7 @@ function renderMessage() {
     }
   }
 
-function renderControls() {
-
-}
-
-
-
+  function renderControls() {
+    // Hids and shows the play again btn
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
+  }
